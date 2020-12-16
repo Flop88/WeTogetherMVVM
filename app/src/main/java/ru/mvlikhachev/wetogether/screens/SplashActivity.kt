6ffb05ca1c23f30592920befc21556c240ab02fc
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-12-16 08:19:29 +0300

    Create splash, navigation logic

---
+++ app/src/main/java/ru/mvlikhachev/wetogether/screens/SplashActivity.kt
@@ -0,0 +1,34 @@
+package ru.mvlikhachev.wetogether.screens
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import ru.mvlikhachev.wetogether.MainActivity
+import ru.mvlikhachev.wetogether.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                try {
+                    sleep(5000)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                } finally {
+                    // Open new fragment
+                    startActivity(
+                        Intent(
+                            this@SplashActivity,
+                            MainActivity::class.java
+                        )
+                    )
+                }
+            }
+        }
+        thread.start()
+    }
+}
