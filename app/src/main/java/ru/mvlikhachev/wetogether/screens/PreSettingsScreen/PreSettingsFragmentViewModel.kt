eb0a6943a9312de1283af5605c3136bc645ccebd
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2021-01-05 21:01:57 +0300

    Loading...

--- app/src/main/java/ru/mvlikhachev/wetogether/screens/PreSettingsScreen/PreSettingsFragmentViewModel.kt
+++ app/src/main/java/ru/mvlikhachev/wetogether/screens/PreSettingsScreen/PreSettingsFragmentViewModel.kt
@@ -1,11 +1,7 @@
 package ru.mvlikhachev.wetogether.screens.PreSettingsScreen
 
 import android.app.Application
-import android.app.DatePickerDialog
-import android.os.Build
 import android.util.Log
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
@@ -14,11 +10,8 @@ import kotlinx.coroutines.launch
 import ru.mvlikhachev.wetogether.database.Room.AppRoomDatabase
 import ru.mvlikhachev.wetogether.database.Room.AppRoomRepository
 import ru.mvlikhachev.wetogether.model.AppPerson
-import ru.mvlikhachev.wetogether.utils.APP_ACTIVITY
 import ru.mvlikhachev.wetogether.utils.REPOSITORY
 import ru.mvlikhachev.wetogether.utils.TYPE_ROOM
-import java.text.SimpleDateFormat
-import java.util.*
 
 class PreSettingsFragmentViewModel(application: Application) : AndroidViewModel(application) {
     private val mContext = application
